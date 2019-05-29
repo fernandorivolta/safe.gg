9f657d80516d5038623ad7c62a7dfb7dee829249
Samuel Alves <samuellmsl05@gmail.com>
2019-05-28 21:14:56 -0300

    Criação da tabela UserFollowPro

--- app/UserFollowPro.php
+++ app/UserFollowPro.php
@@ -4,11 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class UserFollow extends Model
+class UserFollowPro extends Model
 {
+    protected $primaryKey = 'id';
+
     protected $fillable = [
         'id',
         'user_id',
         'proplayer_id'
     ];
+
+    protected $table = 'UserFollowPro';
 }
---
+++ database/migrations/2019_05_29_000912_cria_tabela_userfollowpro.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriaTabelaUserfollow extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('UserFollowPro', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('proplayer_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('Users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('UserFollow');
+    }
+}
