fa285f20568583e8d1d052b44d2c315fc1274646
Enzo Pasianot <enzopasianot@hotmail.com>
2019-06-03 15:08:48 -0300

    viewport para mobile

--- resources/views/lib/head.php
+++ resources/views/lib/head.php
@@ -6,6 +6,9 @@
 			  ███████║██║##██║██║#####███████╗██╗╚██████╔╝╚██████╔╝
 			  ╚══════╝╚═╝##╚═╝╚═╝#####╚══════╝╚═╝#╚═════╝##╚═════╝#
 -->
+<link rel='icon' type='imagem/png' href='/images/icons/owl.png' />
+<title>SAFE.GG</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src='https://code.jquery.com/jquery-3.3.1.js' integrity='sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60='
     crossorigin='anonymous'></script>
 
@@ -30,5 +33,3 @@
 <link rel='stylesheet' href='https://unpkg.com/aos@next/dist/aos.css' />
 <script src='https://unpkg.com/aos@2.3.1/dist/aos.js'></script>
 <script src='/cropit/jquery.cropit.js'></script>
-<link rel='icon' type='imagem/png' href='/images/icons/owl.png' />
-<title>SAFE.GG</title>
